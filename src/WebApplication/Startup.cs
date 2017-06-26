c9bc5a9b32f3e76718b7e32972431137a0f9b847
f7q <kokudou330@gmail.com>
2017-06-27 00:12:25 +0900

    Add TargetFrameworks

--- src/WebApplication/Startup.cs
+++ src/WebApplication/Startup.cs
@@ -1,45 +1,57 @@
 ﻿// Copyright © 2015 Dmitry Sikorsky. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Autofac;
+using Autofac.Core;
+using System;
+using System.Linq;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication
 {
   public class Startup : ExtCore.WebApplication.Startup
   {
-    public Startup(IHostingEnvironment hostingEnvironment)
-      : base(hostingEnvironment)
-    {
-      IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
-        .SetBasePath(hostingEnvironment.ContentRootPath)
-        .AddJsonFile("config.json", optional: true, reloadOnChange: true);
+        public IContainer ApplicationContainer { get; private set; }
+        public Startup(IHostingEnvironment hostingEnvironment)
+          : base(hostingEnvironment)
+        {
+              IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("config.json", optional: true, reloadOnChange: true);
 
-      this.configurationRoot = configurationBuilder.Build();
-    }
+              this.configurationRoot = configurationBuilder.Build();
+        }
 
-    public override void ConfigureServices(IServiceCollection services)
-    {
-      base.ConfigureServices(services);
-    }
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            /*var builder = new ContainerBuilder();
+            //builder.RegisterModule(new AutofacModule());
+            builder.Populate(services);
+            this.ApplicationContainer = builder.Build();*/
+            base.ConfigureServices(services);
+            //return new AutofacServiceProvider(this.ApplicationContainer);
+        }
 
-    public override void Configure(IApplicationBuilder applicationBuilder, IHostingEnvironment hostingEnvironment)
-    {
-      if (hostingEnvironment.IsDevelopment())
-      {
-        applicationBuilder.UseDeveloperExceptionPage();
-        applicationBuilder.UseDatabaseErrorPage();
-        applicationBuilder.UseBrowserLink();
-      }
+        public override void Configure(IApplicationBuilder applicationBuilder, IHostingEnvironment hostingEnvironment)
+        {
+            if (hostingEnvironment.IsDevelopment())
+            {
+                applicationBuilder.UseDeveloperExceptionPage();
+                applicationBuilder.UseDatabaseErrorPage();
+                applicationBuilder.UseBrowserLink();
+            }
 
-      else
-      {
+            else
+            {
 
-      }
+            }
 
-      base.Configure(applicationBuilder, hostingEnvironment);
-    }
+            base.Configure(applicationBuilder, hostingEnvironment);
+        }
   }
 }
--- src/WebApplication/gulpfile.js
+++ src/WebApplication/gulpfile.js
@@ -3,12 +3,12 @@
 gulp.task(
     "copy-extensions-debug", function (cb) {
         gulp.src(["../WebApplication.EFCoreRawQuery/bin/Debug/netstandard1.6/WebApplication.EFCoreRawQuery.dll"]).pipe(gulp.dest("Extensions"));
-        gulp.src(["../WebApplication.NLog/bin/Debug/netstandard1.6/WebApplication.NLog.dll"]).pipe(gulp.dest("Extensions"));
+        gulp.src(["../WebApplication.NLog/bin/Debug/netstandard1.6/WebApplication.NLog.dll"]).pipe(gulp.dest("Commons"));
         gulp.src(["../WebApplication.Elm/bin/Debug/netstandard1.6/WebApplication.Elm.dll"]).pipe(gulp.dest("Extensions"));
         gulp.src(["../WebApplication.ExtensionA/bin/Debug/netstandard1.6/WebApplication.ExtensionA.dll"]).pipe(gulp.dest("Extensions"));
         gulp.src(["../WebApplication.ExtensionB/bin/Debug/netstandard1.6/WebApplication.ExtensionB.dll"]).pipe(gulp.dest("Extensions"));
         gulp.src(["../WebApplication.Script/bin/Debug/netstandard1.6/WebApplication.Script.dll"]).pipe(gulp.dest("Extensions"));
-        gulp.src(["../WebApplication.Swagger/bin/Debug/netstandard1.6/WebApplication.Swagger.dll"]).pipe(gulp.dest("Extensions"));
+        gulp.src(["../WebApplication.Swagger/bin/Debug/netstandard1.6/WebApplication.Swagger.dll"]).pipe(gulp.dest("Commons"));
         gulp.src(["../WebApplication.WebAPI/bin/Debug/netstandard1.6/WebApplication.WebAPI.dll"]).pipe(gulp.dest("Extensions"));
         cb();
     }
